e33c1b351518738deaa47ca77d1767238f8c8bb5
Nolan Flannery <nolanflannery@gmail.com>
2025-03-16 15:09:29 -0700

    added scroll to top on navigate

--- src/components/ContentLayout.tsx
+++ src/components/ContentLayout.tsx
@@ -1,12 +1,15 @@
 import React, { ReactNode } from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
+import { useScrollToTop } from '../hooks/useScrollToTop';
 
 interface ContentLayoutProps {
   children: ReactNode;
 }
 
 export const ContentLayout: React.FC<ContentLayoutProps> = ({ children }) => {
+  useScrollToTop();
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
